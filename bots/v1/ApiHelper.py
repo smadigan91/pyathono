5d5d6002658f9100705cac35e2b8f056b7e0f67b
Sean <pipgargery@gmail.com>
2017-10-26 19:49:02 -0400

    make code slightly less stupid

--- bots/v1/ApiHelper.py
+++ bots/v1/ApiHelper.py
@@ -1,6 +1,14 @@
 from yahoo_oauth import OAuth2
 import xml.etree.ElementTree as ET
 
+#find a better way to do uri templating lol this is gross v
+
+league_idt = ".l.{lid}"
+team_idt = league_idt + ".t.{tid}"
+base_league_url = "https://fantasysports.yahooapis.com/fantasy/v2/league/nba.l.{lid}"
+base_team_url = "https://fantasysports.yahooapis.com/fantasy/v2/team/nba"
+base_player_url = "https://fantasysports.yahooapis.com/fantasy/v2/player/"
+
 #idk why but when using this library this stupid thing is prefixed to each xml tag
 xmlprefix = "{http://fantasysports.yahooapis.com/fantasy/v2/base.rng}"
 
@@ -25,41 +33,34 @@ def ok_get(response):
 
 class ApiHelper:
     
-    league_idt = ".l.{lid}"
-    team_idt = league_idt + ".t.{tid}"
-    base_league_url = "https://fantasysports.yahooapis.com/fantasy/v2/league/nba"
-    base_team_url = "https://fantasysports.yahooapis.com/fantasy/v2/team/nba"
-    base_player_url = "https://fantasysports.yahooapis.com/fantasy/v2/player/"
-    
     def __init__(self, authFileName, leagueId, teamId):
         self.league_id = leagueId
         self.team_id = teamId
-        self.base_league_url += self.league_idt.format(lid=self.league_id)
         self.req = OAuth2(None, None, from_file=authFileName).session
         
     #TODO return convenient data structures
     
     def fetch_league(self, params={}):
-        response = self.req.get(self.base_league_url, params=params)
+        response = self.req.get(base_league_url.format(lid=self.league_id))
         ok_get(response)
         print(response.text)
     
     def fetch_team(self, tid=None, params={}):
         tid = tid if tid is not None else self.team_id
-        response = self.req.get(self.base_team_url + self.get_team_idt(tid), params=params)
+        response = self.req.get(base_team_url + self.get_team_idt(tid), params=params)
         ok_get(response)
         print(response.text)
         
     def fetch_roster(self, tid=None, params={}):
         tid = tid if tid is not None else self.team_id
-        response = self.req.get(self.base_team_url + self.get_team_idt(tid) + "/roster", params=params)
+        response = self.req.get(base_team_url + self.get_team_idt(tid) + "/roster", params=params)
         ok_get(response)
         print(response.text)
         
     # map of {player_name : player_key}
     def fetch_roster_players(self, tid=None, params={}):
         tid = tid if tid is not None else self.team_id
-        response = self.req.get(self.base_team_url + self.get_team_idt(tid) + "/roster/players", params=params)
+        response = self.req.get(base_team_url + self.get_team_idt(tid) + "/roster/players", params=params)
         ok_get(response)
         players = ET.fromstring(response.text)
         ids = {}
@@ -68,12 +69,12 @@ class ApiHelper:
         return ids
         
     def fetch_player_stats(self, player_key, params={}):
-        response = self.req.get(self.base_player_url + player_key + "/stats",params=params)
+        response = self.req.get(base_player_url + player_key + "/stats",params=params)
         ok_get(response)
         print(response.text)
         
     def fetch_player_stats_by_season(self, player_key, season, params={}):
-        response = self.req.get(self.base_player_url + player_key + "/stats;type=season;season={0}".format(season),params=params)
+        response = self.req.get(base_player_url + player_key + "/stats;type=season;season={0}".format(season),params=params)
         ok_get(response)
         print(response.text)
         
@@ -82,13 +83,13 @@ class ApiHelper:
         for pk in player_keys:
             pks += pk + ","
         pks = pks[:-1] + ";"
-        print(self.base_league_url + "/players;player_keys=" + pks + "out=stats")
-        response = self.req.get(self.base_league_url + "/players;player_keys=" + pks + "out=stats",params=params)
+        print(base_league_url + "/players;player_keys=" + pks + "out=stats")
+        response = self.req.get(base_league_url + "/players;player_keys=" + pks + "out=stats",params=params)
         ok_get(response)
         print(response.text)
         
     def get_team_idt(self, tid):
-        return self.team_idt.format(lid=self.league_id, tid=tid)
+        return team_idt.format(lid=self.league_id, tid=tid)
         
     #TODO convenience APIs
 
